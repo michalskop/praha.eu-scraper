c28601ff820195ab362c8ac6b722f98ce56a811a
Michal Skop <michal.skop@kohovolit.eu>
2019-08-07 20:24:31 +0200

    new term

--- scraper.py
+++ scraper.py
@@ -1,18 +1,19 @@
-# scrapes vote events from praha.eu and updates github datapackage
+"""Scrapes vote events from praha.eu and updates github datapackage."""
 
 import csv
-import datapackage  #v0.8.3
+from datapackage import Package  # v0.8.3
 import git
 
 import praha_eu_utils as utils
 import settings
 
 terms = {
-  #'1998-2002': 18280,
-#  '2002-2006': 18281,
-#  '2006-2010': 18282,
-#  '2010-2014': 18284,
-  '2014-2018': 29783,
+    # '1998-2002': 18280,
+    #  '2002-2006': 18281,
+    #  '2006-2010': 18282,
+    #  '2010-2014': 18284,
+    # '2014-2018': 29783,
+    '2018-2022': 33394
 }
 
 resources_attributes = {
@@ -21,7 +22,7 @@ resources_attributes = {
     "votes": ['vote_event_id', 'voter_id', 'option']
 }
 
-path = "data/" # from this script to datapackage.json
+path = "data/"  # from this script to datapackage.json
 
 # repo settings
 repo = git.Repo(settings.git_dir)
@@ -43,33 +44,29 @@ for term in terms:
         "votes": 0
     }
     # get datapackage from github
-    datapackage_url = "https://raw.githubusercontent.com/michalskop/praha.eu-scraper/master/data/" + term +"/datapackage.json"
-    dp = datapackage.DataPackage(datapackage_url)
+    datapackage_url = "https://raw.githubusercontent.com/michalskop/praha.eu-scraper/master/data/" + term + "/datapackage.json"
+    dp = Package(datapackage_url)
 
     # get all vote events
     ves = utils.get_all_vote_events(terms[term])
 
     # get all vote_event_ids from dp and from ves, get links to ves
     dp_ves_ids = []
-    for resource in dp.resources:
-        if resource.descriptor['name'] == 'vote_events':
-            for row in resource.data:
-                dp_ves_ids.append(int(row['id']))
-                ves_table.append(row)
+    for row in dp.get_resource('vote_events').read(keyed=True):
+        dp_ves_ids.append(int(row['id']))
+        ves_table.append(row)
 
     for row in ves:
         ves_ids.append(int(row['id']))
         ves_dict[row['id']] = row
 
     # prepare existing votes
-    for resource in dp.resources:
-        if resource.descriptor['name'] == 'votes':
-            for row in resource.data:
-                votes_table.append(row)
+    for row in dp.get_resource('votes').read(keyed=True):
+        votes_table.append(row)
 
     # download vote event if not exists in dp
     for ve_id in sorted(ves_ids):
-        if not ve_id in dp_ves_ids:
+        if ve_id not in dp_ves_ids:
             ve = utils.get_vote_event(ves_dict[str(ve_id)]['sources:link:url'], ve_id)
             ves_dict[ve_id] = ves_dict[str(ve_id)].update(ve['vote_event'])
             ves_table = ves_table + [ves_dict[str(ve_id)]]
@@ -78,21 +75,25 @@ for term in terms:
             numbers['votes'] += len(ve['votes'])
 
     # update voters
-    for resource in dp.resources:
-        if resource.descriptor['name'] == 'voters':
-            for row in resource.data:
-                voters_dict[row['id']] = row
-                voters_ids.append(int(row['id']))
+    for row in dp.get_resource('voters').read(keyed=True):
+        voters_dict[row['id']] = row
+        voters_ids.append(int(row['id']))
     current_voters = utils.get_current_voters()
     for current_person in current_voters:
         try:
             voters_dict[current_person['id']]
-        except:
+        except Exception:
             voters_ids.append(int(current_person['id']))
             numbers['voters'] += 1
         voters_dict[current_person['id']] = current_person
     for person_id in sorted(voters_ids):
-        voters_table.append(voters_dict[str(person_id)])
+        try:
+            voters_table.append(voters_dict[str(person_id)])
+        except Exception:
+            try:
+                voters_table.append(voters_dict[person_id])
+            except Exception:
+                print(person_id)
 
     # save CSVs
     tables = {
@@ -110,7 +111,7 @@ for term in terms:
             if resource.descriptor['name'] == k:
                 with open(settings.git_dir + path + term + '/' + resource.descriptor['path'], "w") as fout:
                     fieldnames = resources_attributes[k]
-                    csvdw = csv.DictWriter(fout,fieldnames)
+                    csvdw = csv.DictWriter(fout, fieldnames)
                     csvdw.writeheader()
                     for row in tables[k]:
                         csvdw.writerow(row)
